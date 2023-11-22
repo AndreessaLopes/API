88f2384c8b567689e37b586cf35579dcd299bf87
AndreessaLopes <105225463+AndreessaLopes@users.noreply.github.com>
2023-11-22 09:49:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask, request, jsonify
+import base64
 
 app = Flask(__name__)
 
-@app.route('/dobro', methods=['POST'])
-def dobro():
-    # Obtém o valor enviado na requisição POST
-    data = request.get_json()
+@app.route('/processar_imagens', methods=['POST'])
+def processar_imagens():
+    # Obtém a lista de arquivos de imagem enviados na requisição
+    imagens = request.files.getlist('imagens')
 
-    # Verifica se o campo 'valor' está presente na requisição
-    if 'valor' not in data:
-        return jsonify({'error': 'Campo "valor" ausente'}), 400
+    if not imagens:
+        return jsonify({'error': 'Nenhuma imagem recebida'}), 400
 
-    # Obtém o valor a ser dobrado
-    valor = data['valor']
+    resultados = ["ola"]
 
-    # Calcula o dobro do valor
-    resultado = valor * 2
+    for imagem in imagens:
+        # Lê a imagem e converte para base64
+        imagem_bytes = imagem.read()
+        imagem_base64 = base64.b64encode(imagem_bytes).decode('utf-8')
 
-    # Retorna o resultado como JSON
-    return jsonify({'resultado': resultado})
+        # Processa a imagem (aqui você pode fazer o que desejar com a imagem)
+        # Neste exemplo, estamos apenas retornando a representação base64
+        resultados.append(imagem_base64)
+
+    # Retorna os resultados como strings em formato JSON
+    return jsonify({'resultados': resultados})
 
 if __name__ == '__main__':
     # Executa o aplicativo na porta 5000 por padrão
